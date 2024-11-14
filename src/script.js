359d267bf31773b0b833d850d4f74c18290a6800
AlumG <AlumG@majada.es>
2024-11-14 17:56:58 +0100

    modal pending

--- src/script.js
+++ src/script.js
@@ -1,20 +1,31 @@
-const DOMproductList = document.querySelectorAll('#product-list>div');
+const productQty = 5;
+const DOMproductList = document.querySelector('#product-list');
 let fetchList = [];
 
-await fetch('https://fakestoreapi.com/products')
+fetch(`https://fakestoreapi.com/products?limit=${productQty}`)
   .then(res => res.json())
   .then(json => {
     fetchList = json;
+    fetchProducts(json);
   });
 
-console.log(fetchList);
 
-function fetchProducts() {
-  DOMproductList.forEach((e, i) => {
-    e.querySelector('img').src = `${fetchList[i].image}`;
-    e.querySelector('h3').textContent = `${fetchList[i].title}`;
-    e.querySelector('p').textContent = `€${fetchList[i].price}`;
+function fetchProducts(array) {
+  console.log(array);
+  array.forEach((e) => {
+    const article = document.createElement('div');
+    article.classList.add('bg-white', 'dark:bg-gray-800', 'dark:text-white', 'p-6', 'rounded-lg', 'shadow-lg', 'hover:shadow-xl', 'transition-shadow');
+    article.innerHTML = /*html*/ `
+        <img src="${e.image}" alt = "Product 1" class="w-full h-48 object-cover rounded-md mb-4">
+        <h3 class="text-lg font-semibold mb-2">${e.title}</h3>
+        <p class="text-xl mb-4">${e.price}</p>
+        <button class="bg-orange-600 text-white py-2 px-4 rounded-lg hover:bg-orange-500 transition-all">More info</button>
+        `;
+    article.querySelector('button').addEventListener('click', moreInfo, e);
+    DOMproductList.appendChild(article);
   });
 }
 
-fetchProducts();
+function moreInfo(e) {
+  e.target.
+}
